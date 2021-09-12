ab26942f16d916648d80ca33ca5b64523f4c6687
farhoud <farhoud.m7@gmail.com>
2021-09-13 01:54:53 +0430

    - add babel to async-later - rename to mjs to load ems script.

---
+++ libraries/protocols/scripts/build.mjs
@@ -0,0 +1,45 @@
+import esbuild from 'esbuild';
+import path from 'path';
+import babel from 'esbuild-plugin-babel';
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  platform: 'browser',
+  target: ['es6'],
+  format: 'esm',
+  bundle: true,
+  sourcemap: true,
+  plugins: [babel()],
+  outfile: 'dist/browser/esm.js',
+  define: {
+    global: 'globalThis',
+  }
+});
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  platform: 'node',
+  target: 'node14',
+  bundle: true,
+  sourcemap: true,
+  outfile: 'dist/node/cjs.js',
+  define: {
+    global: 'globalThis',
+  },
+  inject: [path.resolve('scripts/node-globals.mjs')],
+});
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  platform: 'node',
+  format:'esm',
+  target: 'node14',
+  bundle: true,
+  sourcemap: true,
+  outfile: 'dist/node/esm.js',
+  define: {
+    global: 'globalThis',
+  },
+  inject: [path.resolve('scripts/node-globals.mjs')],
+});
+

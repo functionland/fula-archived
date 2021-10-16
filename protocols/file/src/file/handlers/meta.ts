ffe9925b3ba5aa36bbb4a577777d92a6cc967120
farhoud <farhoud.m7@gmail.com>
2021-10-16 21:19:15 +0330

    Update protocol handlers to use Stream instead of libp2p Fix import problem of libp2p and peer-id

--- protocols/file/src/file/handlers/meta.ts
+++ protocols/file/src/file/handlers/meta.ts
@@ -1,52 +1,55 @@
 import pipe from 'it-pipe';
-import Libp2p from 'libp2p';
+// @ts-ignore
+import Libp2p, {MuxedStream} from 'libp2p';
+// @ts-ignore
 import PeerId from 'peer-id';
-import { toAsyncIterable } from 'async-later';
-import { Request, Meta } from '../schema';
-import { Response } from '../..';
-import { PROTOCOL } from '../constants';
+import {toAsyncIterable} from 'async-later';
+import {Request, Meta} from '../schema';
+import {Response} from '../..';
+import {PROTOCOL} from '../constants';
 
-type GetMeta = ({ id }: { id: string }) => Response;
+type GetMeta = ({id}: { id: string }) => Response;
 
 const getMetaNotSupported: GetMeta = () => {
-  throw new Error('This node does not support file meta retrieval');
+    throw new Error('This node does not support file meta retrieval');
 };
 
 export let getMeta = getMetaNotSupported;
 
-export function setMetaRetrievalMethod(
-  method: ({ id }: { id: string }) => Promise<Meta | undefined>
-) {
-  getMeta = async ({ id }) => {
-    const meta = await method({ id });
-    const binary = meta && Meta.toBinary(meta);
-    return binary && toAsyncIterable([binary]);
-  };
+export function setMetaRetrievalMethod(method: ({id}: { id: string }) => Promise<Meta | undefined>) {
+    getMeta = async ({id}) => {
+        const meta = await method({id});
+        const binary = meta && Meta.toBinary(meta);
+        return binary && toAsyncIterable([binary]);
+    };
 }
 
-export async function receiveMeta({
-  from,
-  node,
-  id,
-}: {
-  from: PeerId;
-  node: Libp2p;
-  id: string;
-}): Promise<Meta> {
-  const streamReceiveFileMeta = async function* () {
-    yield Request.toBinary({
-      type: {
-        oneofKind: 'meta',
-        meta: id,
-      },
-    });
-  };
-
-  const { stream } = await node.dialProtocol(from, PROTOCOL);
-
-  return pipe(streamReceiveFileMeta, stream, async function (source:any) {
-    for await (const message of source) {
-      return Meta.fromBinary(message.slice());
+export async function receiveMeta({from, connection, id,}: {
+    from: PeerId;
+    connection: { stream: MuxedStream, protocol: string };
+    id: string;
+}): Promise<Meta | Error> {
+    if (connection.protocol !== PROTOCOL) {
+        return Error('Protocol mismatched')
     }
-  });
+    const streamReceiveFileMeta = async function* () {
+        yield Request.toBinary({
+            type: {
+                oneofKind: 'meta',
+                meta: id,
+            },
+        });
+    };
+
+
+    try {
+        return pipe(streamReceiveFileMeta, connection.stream, async function (source: any) {
+            for await (const message of source) {
+                return Meta.fromBinary(message.slice());
+            }
+        });
+    } catch (e) {
+        return e as Error
+    }
+
 }
--- protocols/file/src/file/handlers/retrieve.ts
+++ protocols/file/src/file/handlers/retrieve.ts
@@ -1,52 +1,56 @@
 import pipe from 'it-pipe';
-import Libp2p from 'libp2p';
+import Libp2p, {MuxedStream} from 'libp2p';
 import PeerId from 'peer-id';
-import { Request, Chunk } from '../schema';
-import { Response } from '../..';
-import { PROTOCOL } from '../constants';
+import {Request, Chunk} from '../schema';
+import {Response} from '../..';
+import {PROTOCOL} from '../constants';
 
-type Retrieve = ({ id, skip, limit }: Chunk) => Response;
+type Retrieve = ({id, skip, limit}: Chunk) => Response;
 
 const retrieveNotSupported: Retrieve = () => {
-  throw new Error('This node does not support file content retrieval');
+    throw new Error('This node does not support file content retrieval');
 };
 
 export let retrieve = retrieveNotSupported;
 
 export function setContentRetrievalMethod(method: Retrieve) {
-  retrieve = method;
+    retrieve = method;
 }
 
 export async function* receiveContent({
-  from,
-  node,
-  id,
-  skip,
-  limit,
-}: {
-  from: PeerId;
-  node: Libp2p;
-  id: string;
-  skip?: number;
-  limit?: number;
+                                          connection,
+                                          id,
+                                          skip,
+                                          limit,
+                                      }: {
+    connection: { stream: MuxedStream, protocol: string };
+    id: string;
+    skip?: number;
+    limit?: number;
 }): AsyncIterable<Uint8Array> {
-  const streamReceiveFileContent = async function* () {
-    yield Request.toBinary({
-      type: {
-        oneofKind: 'receive',
-        receive: { id, skip, limit },
-      },
-    });
-  };
-
-  const { stream } = await node.dialProtocol(from, PROTOCOL);
-
-  const chunks = await pipe(streamReceiveFileContent, stream, async function* (source:any) {
-    for await (const message of source) {
-      yield message.slice();
+    if (connection.protocol !== PROTOCOL) {
+        return Error('Protocol mismatched')
     }
-  });
-  for await (const chunk of chunks) {
-    yield chunk;
-  }
+    const streamReceiveFileContent = async function* () {
+        yield Request.toBinary({
+            type: {
+                oneofKind: 'receive',
+                receive: {id, skip, limit},
+            },
+        });
+    };
+
+    try {
+        const chunks = await pipe(streamReceiveFileContent, connection.stream, async function* (source: any) {
+            for await (const message of source) {
+                yield message.slice();
+            }
+        });
+        for await (const chunk of chunks) {
+            yield chunk;
+        }
+    } catch (e) {
+        return e as Error
+    }
+
 }

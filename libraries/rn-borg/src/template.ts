8fa7be56f28a34dde1b3e3e8e6e5e334028cef35
farhoud <farhoud.m7@gmail.com>
2021-11-03 09:34:48 +0330

    remove borg console logs performance issue on logging 3 mb 64base string :D

--- libraries/rn-borg/src/template.ts
+++ libraries/rn-borg/src/template.ts
@@ -1,7 +1,7 @@
 export const template = () => String.raw`<!doctype html>
 <html lang="en">
 <head>
-    <script src="https://cdn.jsdelivr.net/npm/@functionland/rn-borg-bridge@0.2.1/dist/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@functionland/rn-borg-bridge@0.2.2/dist/index.js"></script>
 </head>
 
 </html>`

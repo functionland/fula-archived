6e16bfa15d234afefcbdf6aae5226ed94dacd329
Mahdi Ghorbani <mati.qorb@gmail.com>
2022-01-19 18:58:17 +0100

    update and updateQuery mutation is added.

--- apps/server/src/graphql.ts
+++ apps/server/src/graphql.ts
@@ -1,9 +1,9 @@
-import { GraphQLJSON,GraphQLJSONObject } from 'graphql-type-json';
+import { GraphQLJSON, GraphQLJSONObject } from 'graphql-type-json';
 import { makeExecutableSchema } from 'graphql-tools';
 
 import { getOrbitDb } from '../src/app';
-import {_reGetFilter} from './engine/query'
-
+import { _reGetFilter } from './engine/query'
+import { create, read, update, updateQuery, remove } from './engine'
 const typeDefs = `
     scalar JSON
     scalar JSONObject
@@ -19,6 +19,11 @@ const typeDefs = `
         collection:String!
         values:JSON!
     }
+    input UpdateQueryInput {
+        collection:String!
+        value:JSON!,
+        filter:JSON
+    }
     input DeleteInput {
         collection:String!
         ids:[ID!]
@@ -29,67 +34,22 @@ const typeDefs = `
     type Mutation {
         create (input:CreateInput!):JSON!
         update (input:UpdateInput!):[JSON]
+        updateQuery (input:UpdateQueryInput!):[JSON]
         delete (input:DeleteInput!):[ID!]
     }
 `;
 
 const typeResolvers = {
-  JSON: GraphQLJSON,
-  JSONObject: GraphQLJSONObject,
+    JSON: GraphQLJSON,
+    JSONObject: GraphQLJSONObject,
 };
 
-export const schema = makeExecutableSchema({ typeDefs, resolvers:typeResolvers });
+export const schema = makeExecutableSchema({ typeDefs, resolvers: typeResolvers });
 
 export const resolvers = {
-    read: async ({input}:{input:{collection:string,filter:any}}) => {
-        console.log("read",input);
-        try {
-            const orbitDB = await getOrbitDb();
-            const db = await orbitDB.docs(input.collection);
-            await db.load();
-            // const result = db.get('');
-            const result = db.query(_reGetFilter(input.filter))
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    },
-    create: async ({input}:{input:{collection:string,values:any[]}})=>{
-        console.log("create:",input);
-        try {
-            const orbitDB = await getOrbitDb();
-            const db = await orbitDB.docs(input.collection);
-            await db.load();
-            const _values=input.values.map(value => ({
-                _id: value.id,
-                ...value
-            }))
-            const promises = _values.map(value => db.put({
-                ...value
-            }));
-            const addedList = await Promise.all(promises);
-            console.log('addedList',addedList);
-            return _values;
-            //return new Promise(resolve=>resolve(input.values));
-        } catch (error) {
-            throw error;
-        }
-    },
-    // update: async (collection:string,values:any[])=>{
-        
-    // },
-    remove: async ({input}:{input:{collection:string,ids:string[]}})=>{
-        console.log("remove",input);
-        try {
-            const orbitDB = await getOrbitDb();
-            const db = await orbitDB.docs(input.collection);
-            await db.load();
-            const promises = input.ids.map(id => db.del(id));
-            const deletedList = await Promise.all(promises);
-            console.log(deletedList)
-            return input.ids;
-        } catch (error) {
-            throw error;
-        }
-    }
+    read,
+    create,
+    update,
+    updateQuery,
+    remove
 };

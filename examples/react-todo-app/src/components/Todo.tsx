2b6e97d3ead33c52d8d65f03e78c2686609ff89e
Mahdi Ghorbani <mati.qorb@gmail.com>
2022-01-22 20:53:33 +0100

    init data-protocol example app.

---
+++ examples/react-todo-app/src/components/Todo.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { AiFillCloseCircle } from "react-icons/ai";
+import { AiFillEdit } from 'react-icons/ai';
+export type TODO = {
+  id: string,
+  text: string,
+  isComplete: boolean,
+}
+interface Props {
+  todos: TODO[],
+  completeTodo: (id: string) => void,
+  removeTodo: (id: string) => void,
+  updateTodo: (todo: TODO) => void
+}
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }: Props): JSX.Element => {
+  const [edit, setEdit] = useState<TODO>({
+    id: '',
+    text: '',
+    isComplete: false
+  });
+
+
+  const submitUpdate = (todo: TODO) => {
+    updateTodo({
+      ...todo
+    });
+    setEdit({
+      ...todo
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return <>
+    {
+      todos.map((todo, index) => (
+        <div
+          className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+          key={index}
+        >
+          <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+          </div>
+          <div className='icons'>
+            <AiFillCloseCircle
+              onClick={() => removeTodo(todo.id)}
+              className='delete-icon'
+            />
+            <AiFillEdit
+              onClick={() => setEdit({ ...todo })}
+              className='edit-icon'
+            />
+          </div>
+        </div>
+      ))
+    }
+  </>
+};
+
+export default Todo;
---
+++ examples/react-todo-app/src/components/TodoList.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo, { TODO } from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState<TODO[]>([]);
+
+  const addTodo = (todo: TODO) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+  const updateTodo = (newValue: TODO) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === newValue.id ? newValue : item)));
+  };
+
+  const removeTodo = (id: string) => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id: string) => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>Functionland Todo App</h1>
+      <h4>Exprience graph protocol on the BOX!</h4>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;

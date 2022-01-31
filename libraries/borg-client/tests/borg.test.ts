728c0d4b235d38bc891d6fb6c917052688f3e88f
farhoud <farhoud.m7@gmail.com>
2022-01-31 14:09:31 +0330

    Fix merge and update gitignore

--- libraries/borg-client/tests/borg.test.ts
+++ libraries/borg-client/tests/borg.test.ts
@@ -73,25 +73,6 @@ test('Setup', async function (t) {
       },
       'Meta Most be Same'
     );
-    const query = `mutation {
-                    create(
-                      input: {
-                        collection: "profile"
-                        values: [{ _id: "6", age: 33, name: "jamshid", key: "6" }]
-                      }
-                    ) {
-                      _id
-                      name
-                      age
-                      key
-                    }
-                  }`
-
-    const result = await client.graphql(query)
-    const expected = {
-      data: {create: [{_id: '6', name: 'jamshid', age: 33, key: '6'}]}
-    }
-    t.deepEqual(result, expected, 'Graphql using query')
     t.equal(
       await file.text(),
       await testFile.text(),

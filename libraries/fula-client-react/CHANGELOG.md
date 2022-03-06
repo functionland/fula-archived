5a575c63f724c5869b19891f1f24963cb7a1c71e
farhoud <farhoud.m7@gmail.com>
2022-03-06 19:06:45 +0330

    add changelog remove unused packaged add react and react-dom to externals

---
+++ libraries/fula-client-react/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log - @functionland/fula-client-react
+
+This log was last generated on Sat, 05 Mar 2022 13:27:45 GMT and should not be manually modified.
+
+## 0.1.1
+Sat, 05 Mar 2022 13:27:45 GMT
+
+### Patches
+
+- update dependencies
+
--- libraries/fula-client-react/scripts/build.js
+++ libraries/fula-client-react/scripts/build.js
@@ -1,9 +1,8 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import { build } from 'esbuild';
-import {clean} from 'build-helpers'
+import { clean } from 'build-helpers'
 
 clean()
+
 await build({
   entryPoints: ['src/index.ts'],
   platform: 'browser',
@@ -12,31 +11,10 @@ await build({
   bundle: true,
   sourcemap: true,
   outfile: 'dist/index.js',
-  define: {
-    global: 'globalThis'
-  },
-  plugins: [
-    NodeModulesPolyfillPlugin(),
-    NodeGlobalsPolyfillPlugin({
-      process: true,
-      buffer: true,
-      define: { 'process.env.NODE_ENV': '"dev"' } // inject will override define, to keep env vars you must also pass define here https://github.com/evanw/esbuild/issues/660
-    })
-  ],
   external: [
-    '@functionland/fula'
+    '@functionland/fula','react','react-dom'
   ]
 });
-
-// const b = browserify()
-// b.add('dist/temp.js')
-// const stream = b.bundle()
-// const dest = fs.createWriteStream('dist/index.js')
-// stream.pipe(dest)
-//
-// stream.once('end',()=>{
-//   fs.rmSync('dist/temp.js')
-// })
 
 
 

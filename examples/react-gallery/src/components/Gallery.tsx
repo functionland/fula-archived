5a87d04a51be2d0b3cfe8ac7ae0b10dcc6045508
Aaron Surty <aaron.surty@gmail.com>
2022-03-11 08:21:48 -0500

    display hardcoded list of 'Photo' elements with cid (#60)

--- examples/react-gallery/src/components/Gallery.tsx
+++ examples/react-gallery/src/components/Gallery.tsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import Photo from './Photo';
+import type { PHOTO } from './Photo';
 
 function Gallery() {
+  const [photos, setPhotos] = useState<PHOTO[]>([]);
+  useEffect(() => {
+    setPhotos([{cid:'a'}, {cid:'b'}, {cid:'c'}])
+  });
+
 
   return (
     <>
       <h1>Functionland Sample Gallery</h1>
+    {
+      photos.map((photo, index) => (
+        <div key={index} className='photo'>
+          <Photo photo={photo} />
+        </div>
+      ))
+    }
     </>
   );
 
--- examples/react-gallery/src/components/Photo.tsx
+++ examples/react-gallery/src/components/Photo.tsx
@@ -1,5 +1,18 @@
-export type TODO = {
-  id: string,
-  text: string,
-  isComplete: boolean,
+import React, { useState } from 'react';
+
+export type PHOTO = {
+  cid: string,
 }
+
+interface Props {
+  photo: PHOTO
+}
+
+const Photo = ({ photo, }: Props): JSX.Element => {
+  return <>
+      {photo.cid}
+  </>
+
+};
+
+export default Photo;

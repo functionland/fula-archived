4de6bc6aad19dd0d8cdce78bb709c10bf908dc0a
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-03-13 00:23:11 +0330

    Add subscription resolution to graph-protocol handler

--- protocols/graph/src/handlers/index.ts
+++ protocols/graph/src/handlers/index.ts
@@ -3,22 +3,25 @@ import { partition, firstValue } from 'async-later';
 import { map } from 'streaming-iterables';
 import { ProtocolHandler, Response } from '../';
 import {Request} from '../schema';
-import { resolve } from './resolve';
+import {resolve, subscriptionResolve} from './resolve';
 import { PROTOCOL } from '../constants';
 
 export const handler: ProtocolHandler = async ({ stream }) => {
-  let response: Response = Promise.resolve(Response.EMPTY);
+  let response//: Response = Promise.resolve(Response.EMPTY);
   await pipe(stream, async function (source) {
     const [streamHead, streamTail] = partition(
       1,
       map((message) => message.slice(), source)
     );
     const request = Request.fromBinary(await firstValue(streamHead));
-    response = resolve({query: request.query,operationName: request.operationName, variableValues: request.variableValues})
+    if(!request.subscribe)
+      response = resolve(request)
+    else
+      response = subscriptionResolve(request)
   });
   await pipe((await response) || Response.EMPTY, stream);
   await pipe([], stream); // Close the stream
 };
 
-export { setQueryResolutionMethod, submitQuery } from './resolve';
+export { setQueryResolutionMethod, submitQuery, setSubscriptionQueryResolutionMethod, submitSubscriptionQuery } from './resolve';
 export { PROTOCOL };

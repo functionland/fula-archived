b7509175aacaa5b9dce0de4e78529fc0badb80be
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-03-13 00:22:24 +0330

    Add subscription resolutionMethod

--- protocols/graph/src/handlers/resolve.ts
+++ protocols/graph/src/handlers/resolve.ts
@@ -1,25 +1,34 @@
 import pipe from 'it-pipe';
-import { MuxedStream } from 'libp2p';
+import {MuxedStream} from 'libp2p';
 import {Response} from '../';
-import { PROTOCOL } from '../constants';
+import {PROTOCOL} from '../constants';
 import {Request, Result} from "../schema/graph";
 
 export type Resolve = (Request) => Response;
+export type SubscriptionResolve = (Request) => AsyncIterable<Response>
 
 const resolveNotSupported: Resolve = () => {
     throw new Error('This node does not support graphql');
 };
+const subscriptionResolveNotSupported: SubscriptionResolve = () => {
+    throw new Error('This node does not support subscription over graphql')
+}
 
 export let resolve = resolveNotSupported;
+export let subscriptionResolve = subscriptionResolveNotSupported;
 
 export function setQueryResolutionMethod(method: Resolve) {
     resolve = method;
 }
 
+export function setSubscriptionQueryResolutionMethod(method: SubscriptionResolve) {
+    subscriptionResolve = method
+}
+
 export async function submitQuery({connection, req}: {
     connection: { stream: MuxedStream, protocol: string };
     req: Request
-}): Promise<Result|undefined> {
+}): Promise<Result | undefined> {
     if (connection.protocol !== PROTOCOL) {
         throw new Error('Protocol mismatched')
     }
@@ -35,5 +44,27 @@ export async function submitQuery({connection, req}: {
     });
     for await (const result of queryResult) {
         return Result.fromBinary(result);
+    }
+}
+
+export async function* submitSubscriptionQuery({connection, req}: {
+    connection: { stream: MuxedStream, protocol: string };
+    req: Request
+}): AsyncIterable<Result | undefined> {
+    if (connection.protocol !== PROTOCOL) {
+        throw new Error('Protocol mismatched')
+    }
+    const streamQuerySubmission = async function* () {
+        yield Request.toBinary(req);
+    };
+
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    const queryResult = await pipe(streamQuerySubmission, connection.stream, async function* (source: any) {
+        for await (const message of source) {
+            yield message.slice();
+        }
+    });
+    for await (const result of queryResult) {
+        yield  Result.fromBinary(result);
     }
 }

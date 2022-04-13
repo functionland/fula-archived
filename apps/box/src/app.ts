928ad42aad07677fcb6daf87ca78abed0c9fb92b
farhoud <farhoud.m7@gmail.com>
2022-04-13 13:53:37 +0430

    Fix sync problem

--- apps/box/src/app.ts
+++ apps/box/src/app.ts
@@ -1,22 +1,18 @@
 import Libp2p, {constructorOptions, Libp2pOptions} from 'libp2p';
 import * as IPFS from 'ipfs';
 import { resolveLater } from 'async-later';
-import OrbitDB from 'orbit-db';
 import debug from 'debug';
 import {registerFile} from "./file";
 import {libConfig, ipfsConfig} from "./config";
 import {registerGraph} from "./graph";
-import {IPFS_PATH, ORBITDB_PATH, IPFS_HTTP} from "./const";
+import {IPFS_PATH, IPFS_HTTP} from "./const";
 import config from "config";
 import {create} from "ipfs-http-client";
 
-type DBCollections = {[dbName: string]: any}
 
 
 const [libp2pPromise, resolveLibp2p] = resolveLater<Libp2p>();
 const [ipfsPromise, resolveIpfs] = resolveLater<IPFS.IPFS>();
-const [orbitDBPromise, resolveOrbitDB] = resolveLater<OrbitDB>();
-const [dbCollectionsPromise, resolveDBCollections] = resolveLater<DBCollections>();
 
 export async function getLibp2p() {
   return libp2pPromise;
@@ -24,14 +20,6 @@ export async function getLibp2p() {
 
 export async function getIPFS() {
   return ipfsPromise;
-}
-
-export async function getOrbitDb(){
-  return orbitDBPromise;
-}
-
-export async function getDBCollections(): Promise<DBCollections>{
-  return dbCollectionsPromise;
 }
 
 async function createIPFS(createLibp2p){
@@ -72,20 +60,14 @@ export async function app(config?:Partial<Libp2pOptions&constructorOptions>) {
     createIPFS(createLibp2p)
   )
 
-  resolveDBCollections(new Promise(resolve => {
-    const dbCollections = {}
-    resolve(dbCollections)
-  }))
 
   const libp2pNode = await getLibp2p();
   const ipfsNode = await getIPFS();
   console.log('Box peerID: ' + libp2pNode.peerId.toB58String())
-  resolveOrbitDB(OrbitDB.createInstance(ipfsNode, {directory: ORBITDB_PATH}));
-  const orbitDB= await getOrbitDb();
 
 
   registerFile(libp2pNode, ipfsNode)
-  registerGraph(libp2pNode, orbitDB)
+  registerGraph(libp2pNode, ipfsNode)
   return {
     stop: async () => await graceful()
   }
@@ -94,9 +76,7 @@ export async function app(config?:Partial<Libp2pOptions&constructorOptions>) {
 export async function graceful() {
   debug('\nStopping server...');
   const ipfs = await getIPFS();
-  const orbitDB= await getOrbitDb();
   const libp2p = await getLibp2p();
-  await orbitDB.stop();
   if(!IPFS_HTTP)
     await ipfs.stop();
   await libp2p.stop()
--- apps/box/src/graph/index.ts
+++ apps/box/src/graph/index.ts
@@ -1,81 +1,123 @@
 import {
-    PROTOCOL,
-    handler,
-    setQueryResolutionMethod,
-    Result,
-    Request,
-    setSubscriptionQueryResolutionMethod
+  PROTOCOL,
+  handler,
+  setQueryResolutionMethod,
+  Result,
+  Request,
+  setSubscriptionQueryResolutionMethod
 } from "@functionland/graph-protocol";
 import {createResolver} from "./gql-engine/orbit/orbit-resolvers";
 import {executeAndSelect} from "./gql-engine";
 import {parse} from "graphql";
 import {iterateLater, toAsyncIterable} from "async-later";
-import {getDBCollections} from "../app";
-import * as IPFS from 'ipfs';
+import {ORBITDB_PATH} from "../const";
+import OrbitDB from 'orbit-db';
 
 
+type DBCollections = { [dbName: string]: any }
+const encoder = new TextEncoder()
+const decoder = new TextDecoder()
 
-export const registerGraph = async (libp2pNode ,orbitDBNode) => {
-    libp2pNode.handle(PROTOCOL, handler);
-    const resolvers = createResolver(orbitDBNode)
+export const registerGraph = async (libp2pNode, ipfsNode) => {
+  libp2pNode.handle(PROTOCOL, handler);
 
+  const orbitDBNode = await OrbitDB.createInstance(ipfsNode, {directory: ORBITDB_PATH})
+  const resolvers = createResolver(orbitDBNode)
+  const dbCollections: DBCollections = {}
 
-    const dbCollections = await getDBCollections()
-
-
-    const options = {
-        // Give write access to ourselves
-        accessController: {
-            write: ['*']
-        }
+  const sendDBName = async (message) => {
+    try {
+      const msgString = JSON.stringify(message)
+      const messageBuffer = encoder.encode(msgString)
+      await ipfsNode.pubsub.publish('open-dbs', messageBuffer)
+    } catch (e) {
+      throw (e)
     }
+  }
 
-    const loadDB = async (dbName: string) => {
-        if(dbCollections[dbName]){
-            return dbCollections[dbName]
-        }
-        else {
-            const db = await orbitDBNode.docs('public.'+dbName,options)
-            await db.load()
-            db.events.on('replicated', () => {console.log(`${dbName} replicated`)})
-            db.events.on('peer', (peer) => {console.log(peer)} )
-            dbCollections[dbName] = db
-            return db
-        }
+  const handleDBNameReceived = async (msg) => {
+    if(!msg.topicIDs.includes('open-dbs'))
+      return
+    const rawdata = decoder.decode(msg.data)
+    const data = JSON.parse(rawdata)
+    if(data.ROI && data.ROI === 'req'){
+      await sendDBName({list: [...Object.keys(dbCollections)], ROI: 'res'})
+      return
     }
+    for (const dbName of data.list) {
+      if (!dbCollections[dbName]) {
+        const db = await orbitDBNode.docs(dbName, options)
+        await db.load()
+      }
+    }
+  }
 
-    setQueryResolutionMethod(async function (req: Request) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
+  await ipfsNode.pubsub.subscribe('open-dbs', handleDBNameReceived)
+  await sendDBName({ROI:'req'})
 
-        const {query, variableValues, operationName} = Request.toJson(req)
-        const gqlQuery = parse(query)
-        const data = await executeAndSelect(gqlQuery, resolvers, variableValues, operationName, loadDB)
-        const s = Result.fromJson(data)
-        const bytes = Result.toBinary(s)
-        return bytes && toAsyncIterable([bytes]);
-    })
+  const options = {
+    // Give write access to ourselves
+    accessController: {
+      write: ['*']
+    }
+  }
 
-    setSubscriptionQueryResolutionMethod(async function* (req: Request) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
+  const loadDB = async (dbName: string) => {
+    if (dbCollections[dbName]) {
+      return dbCollections[dbName]
+    } else {
+      const db = await orbitDBNode.docs(dbName, options)
+      await db.load()
+      dbCollections[dbName] = db
+      await sendDBName({list: [...Object.keys(dbCollections)]})
+      if (await ipfsNode.swarm.peers() > 0) {
+        await new Promise(((resolve, reject) => {
+          db.events.once('replicated',
+            () => {
+              resolve('done')
+            }
+          )
+          setTimeout(() => {
+            resolve('done')
+          }, 1000)
+        }))
+      }
+      return db
+    }
+  }
 
-        const {query, variableValues, operationName} = Request.toJson(req)
-        const gqlQuery = parse(query)
+  setQueryResolutionMethod(async function (req: Request) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
 
-        const [values, next, complete] = iterateLater()
+    const {query, variableValues, operationName} = Request.toJson(req)
+    const gqlQuery = parse(query)
+    const data = await executeAndSelect(gqlQuery, resolvers, variableValues, operationName, loadDB)
+    const s = Result.fromJson(data)
+    const bytes = Result.toBinary(s)
+    return bytes && toAsyncIterable([bytes]);
+  })
 
-        const data = await executeAndSelect(gqlQuery, resolvers, variableValues, operationName, loadDB, next, true)
-        const s = Result.fromJson(data)
-        const bytes = Result.toBinary(s)
+  setSubscriptionQueryResolutionMethod(async function* (req: Request) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
 
-        yield bytes
+    const {query, variableValues, operationName} = Request.toJson(req)
+    const gqlQuery = parse(query)
 
-        for await (const res of values){
-            const s = Result.fromJson(res)
-            const bytes = Result.toBinary(s)
+    const [values, next, complete] = iterateLater()
 
-            yield bytes
-        }
-    })
+    const data = await executeAndSelect(gqlQuery, resolvers, variableValues, operationName, loadDB, next, true)
+    const s = Result.fromJson(data)
+    const bytes = Result.toBinary(s)
+
+    yield bytes
+
+    for await (const res of values) {
+      const s = Result.fromJson(res)
+      const bytes = Result.toBinary(s)
+
+      yield bytes
+    }
+  })
 }

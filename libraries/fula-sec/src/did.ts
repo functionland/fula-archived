1547e85a829a5ba387c5a4d205766417635a520f
Mati Q <mahdi.ghorbani@gmail.com>
2022-04-17 20:23:53 +0430

    add uint8arrays,buffer library to fula-sec

--- libraries/fula-sec/src/did.ts
+++ libraries/fula-sec/src/did.ts
@@ -1,8 +1,8 @@
 import { DID } from 'dids'
-// import {generateMnemonic} from 'bip39'
 import {ethers, utils } from 'ethers'
 import KeyResolver from 'key-did-resolver'
 import { Ed25519Provider } from 'key-did-provider-ed25519'
+import {Buffer} from 'buffer';
 /**
  * @class FullaDID
  * @description Creates Decentrilized Identity for Clinet side application 
@@ -17,7 +17,7 @@ interface IFullaDID {
 }
 
 export class FullaDID implements IFullaDID {
-    privateKey!: any
+    privateKey!: string
     mnemonic!: string;
     authDID!: string;
     did: any;
@@ -28,12 +28,11 @@ export class FullaDID implements IFullaDID {
      * @returns  authDID
      */
     private async didProvider () {
-        // const seed = new Uint8Array(32) //  32 bytes with high entropy
-        // const provider = new Ed25519Provider(seed)
-        // this.did = new DID({ provider, resolver: KeyResolver.getResolver() })
-        // // console.log('this.did: ', this.did)
-        // await this.did.authenticate();
-        // console.log('this.did: ', this.did)
+        const pvkey=this.privateKey.replace("0x","")
+        const seed=Buffer.from(pvkey, 'hex');
+        const provider = new Ed25519Provider(seed)
+        this.did = new DID({ provider, resolver: KeyResolver.getResolver() })
+        return this.did.authenticate();
     }
     /**
      * Creates mnemocic phrase and private key
@@ -41,29 +40,15 @@ export class FullaDID implements IFullaDID {
 	 * @returns Object - {authDID, privateKey, mnemonic}
 	 */
     async create () {
-        const seed = new Uint8Array(32) //  32 bytes with high entropy
-        const provider = new Ed25519Provider(seed)
-     
-        const did = new DID({ provider, resolver: KeyResolver.getResolver() })
-        console.log('did: ', did)
-        await did.authenticate();
-
-        console.log('did-id', did.id);
+        const wallet = ethers.Wallet.createRandom()
+        this.mnemonic = wallet.mnemonic.phrase
+        this.privateKey = wallet.privateKey
+        this.authDID = await this.didProvider();
         return {
-            // mnemonic: this.mnemonic,
-            // privateKey: this.privateKey,
-            authDID: did.id
+            mnemonic: this.mnemonic,
+            privateKey: this.privateKey,
+            authDID: this.authDID
         }
-        // const wallet = ethers.Wallet.createRandom()
-        // this.mnemonic = wallet.mnemonic.phrase
-        // this.privateKey = wallet.privateKey
-        // await this.didProvider();
-        // console.log('this.authDID: ', this.did.id)
-        // return {
-        //     // mnemonic: this.mnemonic,
-        //     privateKey: this.privateKey,
-        //     authDID: this.authDID
-        // }
     }
     /**
      * Backup option
@@ -85,8 +70,8 @@ export class FullaDID implements IFullaDID {
 	 */
     async importMnemonic (mnemonic: string) {
         const hdwallet = utils.HDNode.fromMnemonic(mnemonic);
-        this.privateKey = hdwallet.derivePath(`m/44'/60'/0'/0/0`) //.getPrivateKey().toString('hex')
-       await this.didProvider();
+        this.privateKey = hdwallet.derivePath(`m/44'/60'/0'/0/0`).privateKey //.getPrivateKey().toString('hex')
+        this.authDID = await this.didProvider();
         return {
             privateKey: this.privateKey,
             authDID: this.authDID
@@ -100,7 +85,7 @@ export class FullaDID implements IFullaDID {
 	 */
     async importPrivateKey (privateKey: string) {
         this.privateKey = privateKey;
-        await this.didProvider();
+        this.authDID = await this.didProvider();
         return {
             privateKey: this.privateKey,
             authDID: this.authDID

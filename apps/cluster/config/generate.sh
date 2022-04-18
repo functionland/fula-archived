990549e3d66870ae26a0f7b4c5280183077d2cb5
farhoud <farhoud.m7@gmail.com>
2022-04-18 13:06:00 +0430

    Make sure bootstrap list is empty and fix secret creation

--- apps/cluster/config/generate.sh
+++ apps/cluster/config/generate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 if [ ! -d "$DIR" ]; then
 mkdir -p ./config
 fi
 
 echo -e "/key/swarm/psk/1.0.0/\n/base16/\n$(tr -dc 'a-f0-9' < /dev/urandom | head -c64)" > ./config/swarm.key
-export CLUSTER_SECRET=$(tr -dc 'a-f0-9' < /dev/urandom | head -c64)
-echo "$CLUSTER_SECRET"
+export CLUSTER_SECRET=$(od -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
+echo -n -e "$CLUSTER_SECRET"
--- examples/react-gallery/src/components/BoxConfig.jsx
+++ examples/react-gallery/src/components/BoxConfig.jsx
@@ -1,43 +1,84 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
 
-export const BoxConfig = ({serverId, onSet})=>{
+export const BoxConfig = ({serverId, onSet}) => {
 
-    const inputRef = useRef(null);
-    const [_serverId, _setServerId] = useState(serverId)
-    const [selectedFile, setSelectedFile] = useState(null)
+  const inputRef = useRef(null);
+  const [_serverId, _setServerId] = useState(serverId)
+  const [selectedFile, setSelectedFile] = useState(null)
 
-    const handleSelectFile = (event) => {
-        console.log(event.target.files[0])
-        setSelectedFile(event.target.files[0])
-    }
+  const handleSelectFile = (event) => {
+    console.log(event.target.files[0])
+    setSelectedFile(event.target.files[0])
+  }
 
-    const _onSet = (e) => {
-        e.preventDefault();
-        onSet(_serverId.split(','),selectedFile)
-    }
+  const _onSet = (e) => {
+    e.preventDefault();
+    onSet(_serverId.split(','), selectedFile)
+  }
 
-    return  <div className="container">
-        <div className="app-config">
-            <input
-              placeholder='Enter your server Ids comma seperated'
-              value={_serverId}
-              onChange={(e)=>_setServerId(e.target.value)}
-              name='text'
-              ref={inputRef}
-            />
-            <input
-              placeholder='browse key'
-              type="file"
-              onChange={handleSelectFile}
-              name='file'
-            />
-            <button  onClick={_onSet} >
-                Set
-            </button>
+  const [isChecked, setIsChecked] = useState(false);
 
-        </div>
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
 
-    </div>
+  return <>
+    <div className="container">
+      <div className="app-config" style={{
+          display:"flex",
+          flexDirection: "column"
+      }}>
+          <div style={{
+              display:"flex"
+          }}>
+              <input
+                placeholder='Enter your server Ids comma seperated'
+                value={_serverId}
+                onChange={(e) => _setServerId(e.target.value)}
+                name='text'
+                ref={inputRef}
+              />
+              <div>
+                  <button onClick={_onSet}>
+                      Set
+                  </button>
+              </div>
+          </div>
+
+          <div style={{
+
+          }}>
+              <input
+                style={{
+                    width:"auto",
+                    margin: "5"
+                }}
+                type="checkbox"
+                id="topping"
+                name="topping"
+                value="Paneer"
+                checked={isChecked}
+                onChange={handleOnChange}
+              />
+              <label style={{
+                  margin: 0
+              }}>private network</label>
+          </div>
+
+
+        {isChecked && <input
+          placeholder='browse key'
+          type="file"
+          onChange={handleSelectFile}
+          name='file'
+        />}
+      </div>
+
+      </div>
+
+
+
+  </>
 }
 
 

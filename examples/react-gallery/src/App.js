1e6a971698c129a4f93e89ab1b385decbf9e5627
Mehdi Balouchi <mehdibalouchi@gmail.com>
2022-04-28 00:47:28 +0430

    Add enc/dec to react-gallery

--- examples/react-gallery/src/App.js
+++ examples/react-gallery/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
-import React, { useEffect, useState } from 'react';
-import { createClient, Status } from '@functionland/fula';
-import { FullaDID } from '@functionland/fula-sec';
-import { ConnInfo } from './components/ConnInfo';
-import { BoxConfig } from './components/BoxConfig';
-import { Uploader } from './components/Uploader';
-import { Gallery } from './components/Gallery';
-import { Identity } from './components/Identity';
+import React, {useEffect, useState} from 'react';
+import {createClient, Status} from '@functionland/fula';
+import {FulaDID, TaggedEncryption} from '@functionland/fula-sec';
+import {ConnInfo} from './components/ConnInfo';
+import {BoxConfig} from './components/BoxConfig';
+import {Uploader} from './components/Uploader';
+import {Gallery} from './components/Gallery';
+import {Identity} from './components/Identity';
 
 const pages = {
   GALLERY: 'home',
@@ -27,9 +27,11 @@ function App() {
   const [boxIds, setBoxIds] = useState([])
   // List of photos to show
   const [photos, setPhotos] = useState([]);
+  // User DID
+  const [userDID, setDidObj] = useState(null);
 
   const createDID = async () => {
-    const DID = new FullaDID();
+    const DID = new FulaDID();
     const didObj = await DID.create();
     console.log('didObj', didObj);
   };
@@ -49,33 +51,61 @@ function App() {
         const allData = await fula.graphql(readQuery);
         if (allData && allData.data && allData.data.read) {
           setPhotos([]);
-          for (const { cid } of allData.data.read) {
-            const file = await fula.receiveFile(cid);
-            setPhotos((prev) => [...prev, file]);
+          for (const {cid, jwe} of allData.data.read) {
+            let file = null
+            console.log({cid, jwe})
+            if (jwe) {
+              console.log("encrypted photo")
+
+              const tagged = new TaggedEncryption(userDID.did)
+
+              let plainObject
+              try {
+                plainObject = await tagged.decrypt(jwe)
+              } catch (e) {
+                console.log(e)
+                continue
+              }
+
+              const _iv = []
+              for (let i=0; i<16; i+=1)
+                _iv.push(plainObject.symetricKey.iv[i])
+
+              const _symKey = []
+              for (let i=0; i<32; i+=1)
+                _symKey.push(plainObject.symetricKey.symKey[i])
+
+              file = await fula.receiveDecryptedFile(cid, Uint8Array.from(_symKey), Uint8Array.from(_iv))
+            } else {
+              console.log("not encrypted photo")
+              file = await fula.receiveFile(cid);
+            }
+            if(file)
+              setPhotos((prev) => [...prev, file]);
           }
-        }else {
+        } else {
           setPhotos([])
         }
       })();
     }
   }, [page, status, fula]);
 
-  const onSet = (peers,key_file) => {
-    if (peers.length>0) {
+  const onSet = (peers, key_file) => {
+    if (peers.length > 0) {
       setBoxIds(peers);
       (async () => {
         if (peers && peers.length > 0) {
           try {
 
             setConnInfo("")
-            if(fula)
+            if (fula)
               fula.close()
             let _fula
-            if(key_file){
+            if (key_file) {
               const key = await key_file.arrayBuffer()
-              _fula = await createClient({},key)
+              _fula = await createClient({}, key)
               setFula(_fula)
-            }else{
+            } else {
               _fula = await createClient()
               setFula(_fula)
             }
@@ -103,8 +133,18 @@ function App() {
   // and then store its cid in fula data
   const onUpload = async (selectedFile) => {
     try {
-      const cid = await fula.sendFile(selectedFile);
-      await fula.graphql(createMutation, { values: [{ cid, _id: cid }] });
+      // const cid = await fula.sendFile(selectedFile);
+      // await fula.graphql(createMutation, { values: [{ cid, _id: cid }] });
+      const {cid, key} = await fula.sendEncryptedFile(selectedFile)
+      const tagged = new TaggedEncryption(userDID.did)
+
+      let plaintext = {
+        symmetricKey: key,
+        CID: cid
+      }
+
+      let jwe = await tagged.encrypt(plaintext.symmetricKey, plaintext.CID, [userDID.did.id])
+      await fula.graphql(createMutation, {values: [{cid, _id: cid, jwe}]})
       setPhotos((prev) => [selectedFile, ...prev]);
     } catch (e) {
       console.log(e.message);
@@ -113,13 +153,17 @@ function App() {
 
   // call back for routing to setting
 
-  const onSetting = () =>{
+  const onSetting = () => {
     setPage(pages.CONFIG)
+  }
+
+  const onDIDSet = (did) => {
+    setDidObj(did)
   }
 
   return <>
     <div className="app">
-      <Identity/>
+      <Identity onSetting={onDIDSet}/>
       {(() => {
         switch (page) {
           case pages.CONFIG:
@@ -127,8 +171,8 @@ function App() {
           case pages.GALLERY:
             return <>
               <h1>Functionland Sample Gallery</h1>
-              {status===Status.Online && <Uploader onUpload={onUpload}/>}
-              {status!==Status.Connecting && <Gallery photos={photos}/> }
+              {status === Status.Online && <Uploader onUpload={onUpload}/>}
+              {status !== Status.Connecting && <Gallery photos={photos}/>}
             </>
 
           default:
@@ -148,7 +192,10 @@ export const readQuery = `
       collection:"gallery",
       filter:{}
     }){
-      cid
+      cid,
+      symKey,
+      iv,
+      jwe
     }
 
   }

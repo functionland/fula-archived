de279c28b001b1d7ac3f2f70ca640370e5ae96bc
farhoud <farhoud.m7@gmail.com>
2022-07-07 17:59:31 +0430

    Cleanup config and replace our own signaling server

--- libraries/fula-client/src/constant.ts
+++ libraries/fula-client/src/constant.ts
@@ -1,8 +1,7 @@
 import { Multiaddr } from 'multiaddr';
 
 export const SIG_SERVER = [
-  '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
-  '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+  '/dns4/wrtc1.fx.land/tcp/443/wss/p2p-webrtc-star',
 ];
 
 export const SIG_MULTIADDRS = SIG_SERVER.map((item) => {

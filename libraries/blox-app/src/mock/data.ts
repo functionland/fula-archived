6c38347bb786c4622d45c691245a3e5f8ed31ce9
farhoud <farhoud.m7@gmail.com>
2022-08-09 12:07:18 +0430

    add mock data for blox

---
+++ libraries/blox-app/src/mock/data.ts
@@ -0,0 +1,17 @@
+export const BloxMock = {
+    id: "123",
+    totalStorage: 1e+10,
+    poolAllocation: 1e+2,
+    usageStats: [
+        {
+            decentralizedId: "321",
+            storage: 1024
+        },
+        {
+            decentralizedId: "456",
+            storage: 2048
+        }
+    ],
+    poolId: "3216548",
+    towers: ["asd435","asd123"]
+}

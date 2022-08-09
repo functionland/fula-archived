5612f101229afe30c8e947897c15b72a42cf4aee
farhoud <farhoud.m7@gmail.com>
2022-08-09 12:06:34 +0430

    Add friends

--- libraries/blox-app/src/api/User.ts
+++ libraries/blox-app/src/api/User.ts
@@ -1,6 +1,13 @@
-import { User } from "../../spec/interfaces";
+import { User, Friend } from "../../spec/interfaces";
 
 interface UserManager {
+    
     getUser(): User
+
     setUser(u :Partial<User>): boolean
+
+    getFriends(): Friend[]
+
 }
+
+
